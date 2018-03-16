124b480e91f8423ea9c90c4097961c88763570b5
elias <elsdogos@gmail.com>
2018-03-16 22:10:36 +0200

    16-3

---
+++ application/config/myvoucher_config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Name:  Codeigniter voucher library API for  Geniki's web API SOAP/WSDL
+ * Author:  Elias elsdogos
+ * Email: elsdogos@gmail.com
+ * @link https://github.com/eliassdogos/GenikiTaxidromikiVoucher_PHP
+ * @filesource
+ */
+
+
+ // Array because string "user" is common string variable and we want to avoid conflict
+$config['voucher']['user']     = "yourusername";
+$config['voucher']['pass']     = "yourpassword";
+$config['voucher']['api']     = "Your Api";
