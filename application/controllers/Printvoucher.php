7ce67708545983f8dd58039d8a03bd35863c4ef0
elias <elsdogos@gmail.com>
2018-03-18 19:58:03 +0200

    updateController

---
+++ application/controllers/Printvoucher.php
@@ -0,0 +1,80 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Name:  Codeigniter voucher library API for  Geniki's web API SOAP/WSDL
+ * Author:  Elias Sdogos
+ * Email: elsdogos@gmail.com
+ * @link https://github.com/eliassdogos/
+ * @filesource
+ */
+
+class Printvoucher extends CI_Controller 
+{
+       function __construct()
+          {
+              parent::__construct();
+              $this->load->model('voucher_model');// Your CRUD for Order Tables and Vouchers in database
+              $this->load->library(array('my_voucher'));
+              $this->load->config('myvoucher_config');
+          }
+
+  public function index()
+  {
+      
+    
+$mergeprintvoucher=NULL; // Default URL Vouchers to Print are null
+
+$ids=$this->input->get("id");// Get Orders ID from multiple Input Checkbox id[]
+
+$this->my_voucher->connect(); // Connect To Geniki Taxidromiki API and optain second key
+
+$secondkey=$this->my_voucher->assign2key(); // Assign second key to Var
+
+foreach ($ids as $value) {
+    $istherevoucher=$this->voucher_model->YOUR_QUERY_FUNCTION_TO_CHECK($value); // Checking if Order has no Voucher
+                if($istherevoucher->num_rows() > 0){
+foreach (  $istherevoucher->result_array() as $voucherdata){
+    /////Fill The array with tour records from Database
+                    $oVoucher = array(
+                        'OrderId' => $voucherdata["your_column"],
+                        'Name' => $voucherdata["your_column"],
+                        'Address' => 'Test address',
+                        'City' => 'Test city',
+                        'Telephone' => '2109999999',
+                        'Zip' => '12345','Destination' => "",
+                        'Courier' => "",
+                        'Pieces' => 3,
+                        'Weight' => 12,
+                        'Comments' => 'Test comment','Services' => "αν",
+                        'CodAmount' => 1234.56,
+                        'InsAmount' => 0,
+                        'VoucherNumber' => "",
+                        'SubCode' => "",
+                        'BelongsTo' => "",
+                        'DeliverTo' => "",
+                        'ReceivedDate' => "2012-01-01"
+                        );
+$jobid=$this->my_voucher->create_voucher_and_insert($secondkey,$oVoucher,$voucherdata["Columnu_of_Order_id"]);//Assign to variable JobId Result of Voucher Create Method
+$currentvoucher=$this->my_voucher->get_current_voucher();//Geting Voucher Number from current orderId
+$this->voucher_model->insert_vouche_data_to_your_database($currentvoucher$voucherdata["Columnu_of_Order_id"],$jobid);// Insert Voucher Id To Database / Order details // Optional. It depends your Tables and what you want
+$newprintvoucher="&voucherNumbers=".urlencode($currentvoucher);//Assign Current Voucher Number To URL variable "&voucherNumbers="
+    }//End foreach  loop
+    $mergeprintvoucher.=$newprintvoucher;// Merge &voucherNumbers= to URL
+}//End If OrderId has no Voucher
+}///End Loop for OrderID Checked
+
+$secondkeyencoded=urlencode($secondkey);// Encode key string for URL GET Method to Geniki
+$urlforgreekshiper=$this->config->item('urlpost','voucher');// GEting URL to post Voucher
+$urlprintvoucher=$urlforgreekshiper.$secondkeyencoded.$mergeprintvoucher."&Format=Sticker&extraInfoFormat=None";//Setting URL
+redirect($urlprintvoucher);// and Redirect to print PDFS
+
+}///End function index Voucher Create
+
+
+
+
+
+
+
+
+
+}///telos klasis
--- application/library/My_voucher.php
+++ application/library/My_voucher.php
@@ -1,6 +1,4 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
-
-
 /**
  * Name:  Codeigniter voucher library API for  Geniki's web API SOAP/WSDL
  * Author:  Elias Sdogos
@@ -8,8 +6,6 @@
  * @link https://github.com/eliassdogos/
  * @filesource
  */
-
-
 class My_voucher {
 
 public $user;
@@ -17,106 +13,110 @@ public $pass;
 public $api;
 public $url;
 
-	public function __construct()
-	{
-        //Initialize the Library and parse vars from Config
-		      	$this->CI =& get_instance();
-			 	$this->CI->load->config('myvoucher_config');
-			 	$this->user=$this->CI->config->item('user','voucher');
-				$this->pass=$this->CI->config->item('pass','voucher');
-				$this->api=$this->CI->config->item('api','voucher');
-				$this->url=$this->CI->config->item('url','voucher');
-	}
+public function __construct()
+{
+        //Init Variables From Config Here
+                   $this->CI =& get_instance();
+                         $this->CI->load->config('myvoucher_config');
+                         $this->user=$this->CI->config->item('user','voucher');
+                        $this->pass=$this->CI->config->item('pass','voucher');
+                        $this->api=$this->CI->config->item('api','voucher');
+                        $this->url=$this->CI->config->item('url','voucher');
+}
 
-                            public function connect()
-                            {       try { $soap = new SoapClient($this->url);
-                                            $oAuthResult = $soap->Authenticate(
-                                                                        array(
-                                                                                'sUsrName' => $this->user,
-                                                                                    'sUsrPwd' => $this->pass,
-                                                                                        'applicationKey' => $this->api,
-                                                                                                                )
-                                                                                                                        );
-                                            if ($oAuthResult->AuthenticateResult->Result == 0) {
-                                                                $secondpasskey=$this->secondpasskey=$oAuthResult->AuthenticateResult->Key;//Obtain second PassKey
-                                                                                                }
-                                        }///end try
-                                                catch(SoapFault $fault) {
-                                                        echo $fault;
+public function connect()
+{
+        try {
+        $soap = new SoapClient($this->url);
+        $oAuthResult = $soap->Authenticate(
+                array(
+                        'sUsrName' => $this->user,
+                        'sUsrPwd' => $this->pass,
+                        'applicationKey' => $this->api,
+                                                ) );
+
+if ($oAuthResult->AuthenticateResult->Result == 0) {
+                $secondpasskey=$this->secondpasskey=$oAuthResult->AuthenticateResult->Key;
+                return true;//In case if you want to check result from connect function via $varstatusconnection=$this->my_voucher->connect(); in view page
                                                         }
-
-                            }// End Connect Function
-
-
-                            public function assign2key()
-                            {	
-                                    return $this->secondpasskey;
-                                
-                                }//end function assign2key
+        }///end try
+        catch(SoapFault $fault) {
+                echo $fault;
+                }
+}// End Connect Function
 
 
+public function assign2key()
+{	
+        return $this->secondpasskey;
 
-                                public function get_status_for_jobid($key,$jobid)
-                                        {
-                                                $soap = new SoapClient($this->url);
-                                                $xml = array(
-                                                'sAuthKey' => $key,
-                                                'nJobId' => $jobid,//Data
-                                                );
-                                                $resultstatus=$soap->GetVoucherJob($xml);
-                                                return $resultstatus;
-                                        }//end function
-
-
-                        public function cancel_jobid($key,$jobid)
-                        {
-                                $soap = new SoapClient($this->url);
-                                                           $xml = array(
-                                                'sAuthKey' => $key,
-                                                'nJobId' => $jobid,// JobId
-                                                'bCancel' => true, //If cancel parameter is true the job is canceled.
-                                                          );
-                                                $resultcancel=$soap->CancelJob($xml);
-                                                return $resultcancel;
-                                          }//end function
+}//end function assign2key
 
 
 
 
-
-                                public function create_voucher_and_insert($secondkey,$datacustomer,$orderid)
-                                {
-                                $soap = new SoapClient($this->url);
-                                $xml = array(
-                                'sAuthKey' => $secondkey,
-                                'oVoucher' => $datacustomer,//Data Customer is Array() with Order's data
-                                'eType' =>  "Voucher"
-                                );
-                                        $oResult = $soap->CreateJob($xml); //Voucher creation (Usage of the CreateJob method)
-                                        $newvoucher=$this->voucherID=$oResult->CreateJobResult->Voucher; //Initilize Variable voucherID which is voucher number that this job created and assign as public to Variable $newvoucher
-                                        $newvoucherreturn=$this->newvoucherreturn=$newvoucher;//Global Variable To Current Library
-                                        $jobidcreated=$this->voucherID=$oResult->CreateJobResult->JobId;
-                                        return $jobidcreated; //Send Result of Voucher Create Method
-                                          }
+public function get_status_for_jobid($key,$jobid)
+{
+        $soap = new SoapClient($this->url);
+        $xml = array(
+                'sAuthKey' => $key,
+                'nJobId' => $jobid,//Data
+                        );
+        $resultstatus=$soap->GetVoucherJob($xml);
+        return $resultstatus;
+}//end function
 
 
-                                public function get_current_voucher()
-                                          {
-                                             return $this->newvoucherreturn;
-                                                   }//end function
-
-
-                                public function track_and_trace($datav){
-                                         $soap = new SoapClient($this->url);
-                                                $TT = $soap->TrackAndTrace($datav);
-                                                        return $TT;
-                                        }//end function
+public function cancel_jobid($key,$jobid)
+{
+        $soap = new SoapClient($this->url);
+                $xml = array(
+                'sAuthKey' => $key,
+                'nJobId' => $jobid,// JobId
+                'bCancel' => true, //If cancel parameter is true the job is canceled.
+                );
+                $resultcancel=$soap->CancelJob($xml);
+                return $resultcancel;
+}//end function
 
 
 
 
 
+public function create_voucher_and_insert($secondkey,$datacustomer,$orderid)
+{
+        $soap = new SoapClient($this->url);
+        $xml = array(
+                'sAuthKey' => $secondkey,
+                'oVoucher' => $datacustomer,//Data Customer is Array() with Order's data
+                'eType' =>  "Voucher"
+                        );
+        $oResult = $soap->CreateJob($xml); //Voucher creation (Usage of the CreateJob method)
+        $newvoucher=$this->voucherID=$oResult->CreateJobResult->Voucher; //Initilize Variable voucherID which is voucher number that this job created and assign as public to Variable $newvoucher
+        $newvoucherreturn=$this->newvoucherreturn=$newvoucher;//Global Variable  To Current Library
+        $jobidcreated=$this->voucherID=$oResult->CreateJobResult->JobId;
+        return $jobidcreated; //Send Result of Voucher Create Method
+} // end function
 
 
+public function get_current_voucher()
+{
+  return $this->newvoucherreturn;
+}//end fun
 
-}//End Of Class
+
+public function track_and_trace($datav)
+{
+        $soap = new SoapClient($this->url);
+        $TT = $soap->TrackAndTrace($datav);
+        return $TT;
+}//end function
+
+public function close_voucher_untill_now($key)
+{
+                 $soap = new SoapClient($this->url);
+                 $soap->ClosePendingJobs($key);
+}//end function
+
+
+}//End Of library located applications/libraries/
